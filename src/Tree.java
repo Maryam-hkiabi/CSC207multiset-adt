355cc7c94328c036206bb26b800fcea1fee71d94
Maryam-hkiabi <85089679+Maryam-hkiabi@users.noreply.github.com>
2024-05-23 19:59:01 -0400

    changes to Tree.java

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,14 @@
+interface MultiSet {
+        boolean add(Object o);
+        void remove(Object o);
+        boolean contains(Object o);
+        boolean is_empty(Object o);
+        int count(Object o);
+        int size(Object o);
+    }
+
+
 public class Tree {
+        public static void main(String[] args) {}
+
 }
